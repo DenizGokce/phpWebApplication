6db644057b7432048c4cf916b476d095b44e2e35
denizgokce <denizgokce93@gmail.com>
2018-01-30 09:48:03 +0300

    Heroku root Configuration

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://php-my-application.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
